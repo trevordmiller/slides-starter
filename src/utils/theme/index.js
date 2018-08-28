55e5bf53640803632edc4cd69a0280638ebd750a
Trevor D. Miller <trevordmiller@icloud.com>
2018-08-27 19:56:02 -0600

    Style inline code

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,5 +1,5 @@
 import theme from "mdx-deck/themes";
-import colorValues, { uiGroups } from "nova-colors";
+import colorValues, { uiGroups, syntaxGroups } from "nova-colors";
 import okaidia from "react-syntax-highlighter/styles/prism/okaidia";
 import prismJavaScript from "react-syntax-highlighter/languages/prism/javascript";
 import prismJsx from "react-syntax-highlighter/languages/prism/jsx";
@@ -14,8 +14,7 @@ const customTheme = {
   blockquote: {
     color: uiGroups.gray4,
     fontStyle: "italic",
-    fontWeight: 400,
-    fontSize: "inherit"
+    fontWeight: 400
   },
   colors: {
     text: uiGroups.gray5,
@@ -23,6 +22,7 @@ const customTheme = {
     heading: uiGroups.userCurrentState,
     preBackground: "rgba(0, 0, 0, 0.25)",
     codeBackground: "transparent",
+    code: syntaxGroups.statement,
     link: colorValues.colors.blue
   },
   prism: {
