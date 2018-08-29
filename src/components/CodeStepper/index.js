8992b4157fa5c5463b5b722a8d372ef23e58e471
Trevor D. Miller <trevordmiller@icloud.com>
2018-08-28 20:35:25 -0600

    Add theme to code stepper

--- src/components/CodeStepper/index.js
+++ src/components/CodeStepper/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { CodeSurfer } from "mdx-deck-code-surfer";
+import theme from "prism-react-renderer/themes/duotoneDark";
 
 const CodeStepper = ({ code, steps }) => (
-  <CodeSurfer code={code} steps={steps} />
+  <CodeSurfer code={code} steps={steps} theme={theme} />
 );
 
 export default CodeStepper;
